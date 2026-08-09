a43324c3966ef877de3fc5d1a660d3be3cd54762
shoxjum556-lgtm <shoxjum556@gmail.com>
2026-08-09 12:30:14 +0500

    Add dynamic CSRF_TRUSTED_ORIGINS configuration

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ RAILWAY_HOSTNAME = os.getenv("RAILWAY_PUBLIC_DOMAIN")
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [f"https://{h}" for h in [RENDER_HOSTNAME, RAILWAY_HOSTNAME] if h] 
 CSRF_TRUSTED_ORIGINS += ["https://*.up.railway.app", "https://*.onrender.com", "https://bozorch-production.up.railway.app"]
 
 INSTALLED_APPS = [
